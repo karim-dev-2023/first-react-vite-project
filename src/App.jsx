03b38fed6c863ad9ab8b34bb9677d2f57d527d21
Karim <karim.ayoub.job@gmail.com>
2025-03-03 14:57:44 +0100

    Add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
     <>
       <Header />
       <h1>Hello </h1>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import "../styles/Footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>Copyright Mexican Food 2030</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
