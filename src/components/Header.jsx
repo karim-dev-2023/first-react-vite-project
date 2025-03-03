c79f5a528dfe37078c9b925e779e3f6d8e6fd630
Karim <karim.ayoub.job@gmail.com>
2025-03-03 14:25:00 +0100

    Add a header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import '../styles/Header.css'
+
+function Header(){
+    return(
+        <>
+        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Logo"/>
+        <a href="#">Page Accueil</a>
+        </>
+    );
+}
+
+export default Header;
