15e3ed3181c5d893c824308c86d7785bab128868
Karim <karim.ayoub.job@gmail.com>
2025-03-03 14:31:43 +0100

    Fix Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/Header.jsx";
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Hello </h1>
     </>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
-import '../styles/Header.css'
+import "../styles/Header.css";
 
-function Header(){
-    return(
-        <>
-        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="Logo"/>
-        <a href="#">Page Accueil</a>
-        </>
-    );
+function Header() {
+  return (
+    <>
+      <header>
+        <img
+          src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+          alt="Logo"
+        />
+        <nav>
+          <a href="#">Page Accueil</a>
+        </nav>
+      </header>
+    </>
+  );
 }
 
 export default Header;
